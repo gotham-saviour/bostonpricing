9175b63d2e704e3a6dcdcefa59e1229f80a37694
gotham-saviour <bhaveshs11@gmail.com>
2024-09-05 18:45:01 +0530

    deployment commit

--- app.py
+++ app.py
@@ -10,12 +10,19 @@ scaler=pickle.load(open('scaling.pkl','rb'))
 def home():
     return render_template('home.html')
 @app.route('/predict_api',methods=['POST'])
-
 def predict_api():
     data=request.json("data")
     print(data)
     new_data=scaler.transform(data)
     output=regression.predict(new_data)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values]
+    final_input=scaler.transform(np.array(data).reshape(-1,1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text=f"The house price prediction is {}")
 
 if __name__=="__main__":
     app.run(debug=True)
